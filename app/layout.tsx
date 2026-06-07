f74a629ac715d9348e3327b461b67d81dcce6c53
George Featherstone <george@devcabin.com>
2026-06-07 14:12:08 -0500

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" href="/duotron_favicon.png" />
         <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,400;0,700;0,900;1,400&family=Barlow:wght@400;500;600&display=swap" rel="stylesheet" />
       </head>
       <body>{children}</body>

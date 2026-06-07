bbca68905905f045bee08da28ca5710b7f2fa43a
George Featherstone <george@devcabin.com>
2026-06-07 14:36:39 -0500

    fix: link DevCabin to devcabin.com

--- app/page.tsx
+++ app/page.tsx
@@ -220,7 +220,7 @@ export default function Home() {
         }}>
           A Research Project By{' '}
           <a 
-            href="https://github.com/DevCabin"
+            href="https://devcabin.com"
             target="_blank"
             rel="noopener noreferrer"
             style={{
